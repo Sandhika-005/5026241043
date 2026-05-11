9166264d3898a32f758d068bd68c86a93869061b
Sndhka.99.5 <sandhikamuhammad31@gmail.com>
2026-05-11 11:43:48 +0700

    pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
-use App\Http\Controllers\PengawaiController;
+use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,9 +27,9 @@ Route::get('/dosen', [DosenController::class, 'index']);
 Route::get('/biodata', [DosenController::class, 'biodata']);
 
 //pertemuan 9
-Route::get('/pengawai/{nama}', [PengawaiController::class, 'index']);
-Route::get('/formulir', [PengawaiController::class, 'formulir']);
-Route::post('/formulir/proses', [PengawaiController::class, 'proses']);
+Route::get('/pegawainama/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 //route blog
 Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
@@ -80,3 +81,7 @@ Route::get('/tugas/pertemuan-3.1', function () {
 Route::get('/tugas/pertemuan-2.1', function () {
     return view('tugas.news-1');
 });
+
+
+// Pertemuan 11
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
